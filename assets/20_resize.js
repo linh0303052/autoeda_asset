d788197129f6ce06272706b0dab288095d3c55f5
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2022-12-07 10:39:35 +0700

    Add files via upload

---
+++ assets/20_resize.js
@@ -0,0 +1,40 @@
+$(function () {
+    var pressed = false;
+    var start = undefined;
+    var startX, startWidth;
+    var widthDefault = $("#wrapper").width();
+    var minWidthResize = $("#resize").width();
+
+    //max width of first thead
+
+    console.log("startWidth", startWidth);
+
+    startWidth = $("#resize").mousedown(function (e) {
+        start = $(this);
+        pressed = true;
+        startX = e.pageX;
+        startWidth = $(this).width();
+        if (minWidthResize > 100) {
+            $(start).addClass("resizing");
+        }
+    });
+
+    $(document).mousemove(function (e) {
+        if (pressed) {
+            //100 is min width resize
+            if (e.pageX - startX > 0 || startWidth + e.pageX - startX > 100) {
+                $("#resize").width(startWidth + (e.pageX - startX));
+                $("#wrapper").width(
+                    widthDefault + startWidth + (e.pageX - startX)
+                );
+            }
+        }
+    });
+
+    $(document).mouseup(function () {
+        if (pressed) {
+            $(start).removeClass("resizing");
+            pressed = false;
+        }
+    });
+});
